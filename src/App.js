3160acafe66617d2c69c331cd98da3ca7df2c6ae
pemauric <pedro.a.mauricio1@gmail.com>
2023-09-04 18:43:24 -0300

    added components in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/layouts/Navbar";
+import Footer from "./components/layouts/Footer";
+
+import Login from "./components/pages/Auth/Login";
+import Register from "./components/pages/Auth/Register"
+import Home from "./components/pages/Home";
 
 
 function App() {
   return (
-    <div className="App">
-      <h1>Get A Pet</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />}/>
+        <Route path="/register" element={<Register />}/>
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ src/components/layouts/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <footer>
+            <p>
+                <span class="bold">Get A Pet</span> &copy; 2023
+            </p>
+        </footer>
+    )
+}
+
+export default Footer;
