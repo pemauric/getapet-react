ae6e2d9b5b467a2f937eedbbe6d2e98dd10be96e
pemauric <pedro.a.mauricio1@gmail.com>
2023-09-05 12:25:46 -0300

    added footer, navbar and container

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -2,13 +2,15 @@ import { Link } from "react-router-dom";
 
 import Logo from "../../assets/img/logo.png";
 
+import styles from "./Navbar.module.css"
+
 function Navbar() {
     return (
-        <nav>
-            <div>
+        <nav className={styles.navbar}>
+            <div className={styles.navbar_logo}>
                 <img src={Logo} alt="Get A Pet" />
+                <h2>Get A Pet</h2>
             </div>
-            <h2>Get A Pet</h2>
             <ul>
                 <li>
                     <Link to={"/"}>Adotar</Link>
